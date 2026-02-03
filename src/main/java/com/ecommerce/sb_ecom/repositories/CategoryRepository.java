7376acf81308fec904a7baec62f39c787608783d
manirathnam5 <manirathnam501@gmail.com>
2026-02-03 15:23:36 +0530

    added repository for category

---
+++ src/main/java/com/ecommerce/sb_ecom/repositories/CategoryRepository.java
@@ -0,0 +1,10 @@
+package com.ecommerce.sb_ecom.repositories;
+
+import com.ecommerce.sb_ecom.model.Category;
+import org.springframework.data.repository.CrudRepository;
+
+ public interface CategoryRepository  extends CrudRepository<Category,Long> {
+
+
+
+ }
